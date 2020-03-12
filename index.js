0181f18ca6569d55992921a0837f15e3247fed40
Zach Johnson <zjohnson@adobe.com>
2020-03-12 00:44:50 -0600

    too much

--- index.js
+++ index.js
@@ -18,83 +18,55 @@ server.post('/restartImage', (req, res) => {
 	const image = req.body.repository.repo_name + ':' + req.body.push_data.tag;
 	console.log(image);
 	try{
-		docker.listImages((err, images) => {
-			const imagesToDelete = [];
-			for(let localImage of images){
-				let relatedImage = false;
-				for(let tag of localImage.RepoTags){
-					if(tag.split(':'[0] === localImage.split(':'))){
-						relatedImage = true;
-					}
-				}
-				if(relatedImage && !localImage.includes(image)){
-					imagesToDelete.push(localImage.Id);
-				}
+		docker.listContainers(function (err, containers) {
+			let containerSearched = containers.length;
+			if(containers.length === 0){
+				return res.status(404).send('No containers running to stop');
 			}
+			let containerToKill = null;
+			for(let containerInfo of containers){
+				console.log(`looking at ${containerInfo.Image}`);
+				if(containerInfo.Image === image){
+					console.log('found: ' + JSON.stringify(containerInfo));
+					containerToKill = containerInfo.Id;
 
-			docker.listContainers(function (err, containers) {
-				let containerSearched = containers.length;
-				if(containers.length === 0){
-					return res.status(404).send('No containers running to stop');
 				}
-				let containerToKill = null;
-				for(let containerInfo of containers){
-					console.log(`looking at ${containerInfo.Image}`);
-					if(containerInfo.Image === image){
-						console.log('found: ' + JSON.stringify(containerInfo));
-						containerToKill = containerInfo.Id;
-					}
-					containerSearched--;
-					if(containerSearched===0 ){
-						docker.pull(image, (err, stream) => {
-							stream.on('data', (chunk) => {
-								console.log(chunk.toString());
-							});
-							stream.on('end', () => {
-								if(containerToKill !== null ){
+				containerSearched--;
+				if(containerSearched===0 ){
+					docker.pull(image, (err, stream) => {
+						stream.on('data', (chunk) => {
+							console.log(chunk.toString());
+						});
+						stream.on('end', () => {
+							if(containerToKill !== null ){
 
-									docker.getContainer(containerToKill).kill(() => {
-										fetch(req.body.callback_url, {
-											method: "post",
-											headers: {
-												"Content-type": "application/json",
-												"Accept": "application/json",
-												"Accept-Charset": "utf-8"
-											},
-											body: JSON.stringify({
-												state: "success"
-											})
-										}).then((response) => {
-											let deletedImages = 0;
-											if(imagesToDelete.length === 0){
-												return res.json({status: 'success'});
-											}
-											for(let imageToDelete of  imagesToDelete){
-												docker.getImage(imageToDelete).remove(() => {
-													deletedImages++;
-													if(deletedImages === imagesToDelete.length){
-														return res.json({status: 'success'});
-													}
-												});
-											}
-
-										});
+								docker.getContainer(containerToKill).kill(() => {
+									fetch(req.body.callback_url, {
+										method: "post",
+										headers: {
+											"Content-type": "application/json",
+											"Accept": "application/json",
+											"Accept-Charset": "utf-8"
+										},
+										body: JSON.stringify({
+											state: "success"
+										})
+									}).then((response) => {
+										return res.json({status: 'success'});
 									});
+								});
 
-								}
-								else{
-									return res.status(404).send('Containers with image not found');
-								}
-							});
-
+							}
+							else{
+								return res.status(404).send('Containers with image not found');
+							}
 						});
 
-					}
+					});
+
 				}
-			});
-
+			}
 		});
-
 
 	}
 	catch(e){
